8436a968655a51098f938b886fbdef1a04c022c6
sebastianmoiras <Jonathansebastian858@gmail.com>
2025-09-18 01:38:16 +0700

    Update jwt_service.py

--- Back-End/services/jwt_service.py
+++ Back-End/services/jwt_service.py
@@ -1,11 +1,7 @@
 import jwt
 import datetime
 from fastapi import HTTPException, status
-#jwt token itu biar user dikasih token, agar website mengenali nih user udah lewat proses login, 
-#jadi mau pake fitur apapun di website itu udah gaperlu verifikasi karena ada key yang nempel
 
-#secret key adalah key yang digunakan untuk 'tanda tangan' kepada user yang login, jadi ketika sudah diverified
-#user diberikan 1 token untuk memberi tau website bahwa user boleh mengakses website sampai masa token expired
 SECRET_KEY = "tokenJonathan2702269633SebastianMoiras"
 ALGORITHM = "HS256"
 
@@ -26,3 +22,4 @@ def verify_token(token: str):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token expired")
     except jwt.InvalidTokenError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+
