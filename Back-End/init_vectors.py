579d5f76e7add0664d12f071036df6be2066f15b
sebastianmoiras <Jonathansebastian858@gmail.com>
2025-09-18 01:33:29 +0700

    Update init_vectors.py

--- Back-End/init_vectors.py
+++ Back-End/init_vectors.py
@@ -2,5 +2,6 @@ from services.vector_service import build_embeddings, collection
 
 if __name__ == "__main__":
     build_embeddings()
-    print("✅ Embeddings berhasil dibuat dan disimpan ke Chroma")
+    print("Embeddings berhasil dibuat dan disimpan ke Chroma")
+
     print("Total film di vector store:", collection.count())
